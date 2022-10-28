9f23f4d18ef544fa925f1ad327101efcac685002
Andres Martinez <andresf.mrh@gmail.com>
2022-10-28 14:46:50 -0500

    form fields and elements added

--- PeopleProject/Person.cpp
+++ PeopleProject/Person.cpp
@@ -1,2 +1,12 @@
 #include "Person.h"
+using namespace System;
+using namespace System::Windows::Forms;
+ [STAThread]
+
+ void main(array<String^>^ args) {
+	 Application::EnableVisualStyles();
+	 Application::SetCompatibleTextRenderingDefault(false);
+	 PeopleProject::Person form;
+	 Application::Run(% form);
+}
 
--- PeopleProject/Person.h
+++ PeopleProject/Person.h
@@ -34,6 +34,20 @@ namespace PeopleProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker1;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +62,130 @@ namespace PeopleProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Person";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(230, 69);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(179, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Person::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(305, 53);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(44, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Nombre";
+			this->label1->Click += gcnew System::EventHandler(this, &Person::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(285, 109);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(76, 13);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Primer Apellido";
+			this->label2->Click += gcnew System::EventHandler(this, &Person::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(285, 169);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(90, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Segundo Apellido";
+			this->label3->Click += gcnew System::EventHandler(this, &Person::label3_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(282, 232);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(93, 13);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"Fecha Nacimiento";
+			this->label4->Click += gcnew System::EventHandler(this, &Person::label4_Click);
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->button1->Location = System::Drawing::Point(285, 297);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(76, 25);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"Enviar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(230, 135);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(179, 20);
+			this->textBox2->TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(230, 196);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(179, 20);
+			this->textBox3->TabIndex = 10;
+			// 
+			// dateTimePicker1
+			// 
+			this->dateTimePicker1->Location = System::Drawing::Point(230, 258);
+			this->dateTimePicker1->Name = L"dateTimePicker1";
+			this->dateTimePicker1->Size = System::Drawing::Size(179, 20);
+			this->dateTimePicker1->TabIndex = 12;
+			// 
+			// Person
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(616, 365);
+			this->Controls->Add(this->dateTimePicker1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"Person";
+			this->Text = L"Person";
+			this->Load += gcnew System::EventHandler(this, &Person::Person_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void Person_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
